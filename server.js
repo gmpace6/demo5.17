cd59845ed1df2a82b5894316682ce518c727e805
Garrett Pace <elderpace6@gmail.com>
2023-05-17 13:07:42 -0600

    ready for AWS

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ app.use(express.static(__dirname + '/public'))
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/public/index.html')
 // })
-
 // app.get('/styles', (req, res) => {
 //     res.sendFile(__dirname + '/public/styles.css')
 // })
-
 // app.get('/js', (req, res) => {
 //     res.sendFile(__dirname + '/public/main.js')
 // })
